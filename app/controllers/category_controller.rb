cd49de04d04b13042feab3c64ace76e6048deffc
Josh <jmharvey0@gmail.com>
2017-07-13 17:01:02 -0500

    add categories show page to view the styles

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -2,3 +2,8 @@ get '/categories' do
   @categories = Category.all
   erb :"categories/index"
 end
+
+get '/categories/:id' do
+  @category = Category.find(params[:id])
+  erb :'categories/show'
+end
