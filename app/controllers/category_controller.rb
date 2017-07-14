6484fc64ed3212cfc05789497c8e40ec1aa37150
Josh Harvey <jmharvey0@gmail.com>
2017-07-13 19:49:04 -0500

    api requests working for all categories

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -2,14 +2,15 @@ before do
   require_relative '../models/brewery_db_adapter'
 end
 get '/categories' do
-  p ENV['BREWERY_DB_ACCESS_TOKEN']
-  p brewery_db_categories = BreweryDBAdapter.new(ENV['BREWERY_DB_ACCESS_TOKEN'])
-  # @categories =
-  @categories = Category.all
+  brewery_db = BreweryDBAdapter.new(ENV['BREWERY_DB_ACCESS_TOKEN'])
+  @categories = brewery_db.categories
+  # @categories = Category.all
   erb :"categories/index"
 end
 
 get '/categories/:id' do
-  @category = Category.find(params[:id])
+  brewery_db = BreweryDBAdapter.new(ENV['BREWERY_DB_ACCESS_TOKEN'])
+  @category = brewery_db.category_find(params[:id])
+  # @category = Category.find(params[:id])
   erb :'categories/show'
 end
--- app/models/brewery_db_adapter.rb
+++ app/models/brewery_db_adapter.rb
@@ -7,6 +7,10 @@ class BreweryDBAdapter
   end
 
   def categories
-    self.class.get("/categories", @options)
+    self.class.get("/categories", @options)['data'].map{|args| Category.new(id: args['id'], name: args['name'])}
+  end
+  def category_find(finder)
+    response = self.class.get("/category/#{finder}", @options)['data']
+    Category.new(id: response['id'], name: response['name'])
   end
 end
