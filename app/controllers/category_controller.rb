426340348bc7e378da59dbff06ee977d1bd219dc
Josh Harvey <jmharvey0@gmail.com>
2017-07-14 11:11:48 -0500

    remove comments from category_controller

--- app/controllers/category_controller.rb
+++ app/controllers/category_controller.rb
@@ -1,16 +1,9 @@
-# before do
-#   require_relative '../models/brewery_db_adapter'
-# end
 get '/categories' do
-  # brewery_db = BreweryDBAdapter.new(ENV['BREWERY_DB_ACCESS_TOKEN'])
-  # @categories = brewery_db.categories
   @categories = Category.all
   erb :"categories/index"
 end
 
 get '/categories/:id' do
-  # brewery_db = BreweryDBAdapter.new(ENV['BREWERY_DB_ACCESS_TOKEN'])
-  # @category = brewery_db.category_find(params[:id])
   @category = Category.find(params[:id])
   erb :'categories/show'
 end
